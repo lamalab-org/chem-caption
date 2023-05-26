9c0012a7c1259dc08e709feef5a6afde85e17da0
Arkhymadhe <emoekabuoshomah@gmail.com>
2023-05-26 16:13:14 +0100

    Implement getter for molecular mass; move representation identifier to abstract MoleculeBase class

--- src/chemcaption/api.py
+++ src/chemcaption/api.py
@@ -7,8 +7,6 @@
 import rdkit
 from rdkit import Chem
 from abc import abstractmethod, ABC
-from fastcore.all import typedispatch
-from multipledispatch import dispatch
 from selfies import decoder, encoder
 
 from collections import namedtuple
@@ -18,9 +16,15 @@ from collections import namedtuple
 
 
 class MoleculeBase(ABC):
+    def __init__(self, repr_type):
+        self.repr_type = repr_type
+
     @abstractmethod
     def get_rdkit_mol(self):
         raise NotImplementedError
+
+    def get_name(self):
+        return self.repr_type
 
 
 class AbstractFeaturizer(ABC):
@@ -73,9 +77,9 @@ class InChIMolecule(MoleculeBase):
 
 
 class Molecule(MoleculeBase):
-    def __init__(self, base_string, rep_type="smiles"):
-        self.base_string = base_string
-        self.rep_type = rep_type.lower()
+    def __init__(self, repr_string, repr_type="smiles"):
+        super(Molecule, self).__init__(**dict(repr_type=repr_type))
+        self.repr_string = repr_string
 
         self.dispatch_map = {
             "smiles": SMILESMolecule,
@@ -83,17 +87,18 @@ class Molecule(MoleculeBase):
             "inchi": InChIMolecule,
         }
 
-        self.molecule = self.dispatch_map[self.rep_type](self.base_string)
+        self.molecule = self.dispatch_map[self.repr_type](self.repr_string)
         self.rdkit_mol = self.get_rdkit_mol()
 
     def get_rdkit_mol(self):
         return self.molecule.get_rdkit_mol()
 
-    def get_name(self):
-        return self.rep_type
-
     def get_atoms(self):
         return self.rdkit_mol.GetAtoms()
+
+    def reveal_hydrogens(self, **kwargs):
+        self.rdkit_mol = Chem.rdmolops.AddHs(self.rdkit_mol, **kwargs)
+        return None
 
 
 class MoleculeFeaturizer(AbstractFeaturizer):
@@ -130,8 +135,11 @@ class MoleculeFeaturizer(AbstractFeaturizer):
         return None
 
     def get_element_info(self, molecule):
+        molecule.reveal_hydrogens()
+        atoms_info = namedtuple("information", ["element", "symbol", "atomic_number"])
+
         atoms_info = [
-            (
+            atoms_info(
                 periodic_table.GetElementName(atom.GetAtomicNum()),
                 periodic_table.GetElementSymbol(atom.GetAtomicNum()),
                 periodic_table.GetAtomicWeight(atom.GetAtomicNum()),
@@ -141,33 +149,35 @@ class MoleculeFeaturizer(AbstractFeaturizer):
 
         return atoms_info
 
-    def _to_selfies(self, molecule):
-        repr_kind = molecule.base_string
-        rep_type = molecule.rep_type
-
-        if rep_type == "selfies":
-            return molecule
-        else:
-            if rep_type == "inchi":
-                repr_kind = Chem.MolToSmiles(molecule.get_rdkit_mol())
-                repr_kind = encoder(repr_kind)
-
-        return Molecule(repr_kind, "selfies")
-
-    def get_molar_mass(self, atomic_info=None, molecule=None,):
-        if atomic_info is None:
-            atomic_info = self.get_element_info(
-                self._to_selfies(molecule)
-            )
-        molar_mass = sum([TUPLE[-1] for TUPLE in atomic_info])
-        return molar_mass
-
     def get_unique_elements(self, atomic_info=None, molecule=None):
         if atomic_info is None:
             atomic_info = self.get_element_info(molecule)
 
         unique_atoms = [TUPLE[0] for TUPLE in set(atomic_info)]
         return unique_atoms
+
+    def _to_selfies(self, molecule):
+        repr_kind = molecule.repr_string
+        repr_type = molecule.repr_type
+
+        if repr_type == "selfies":
+            return molecule
+        else:
+            if repr_type == "inchi":
+                repr_kind = Chem.MolToSmiles(molecule.get_rdkit_mol())
+                repr_kind = encoder(repr_kind)
+
+        return Molecule(repr_kind, "selfies")
+
+    def get_molar_mass(
+        self,
+        atomic_info=None,
+        molecule=None,
+    ):
+        if atomic_info is None:
+            atomic_info = self.get_element_info(self._to_selfies(molecule))
+        molar_mass = sum([TUPLE.atomic_number for TUPLE in atomic_info])
+        return molar_mass
 
     def get_element_frequency(self, element, molecule=None, atomic_info=None):
         if atomic_info is None:
@@ -177,7 +187,9 @@ class MoleculeFeaturizer(AbstractFeaturizer):
 
         element_count = len(
             [
-                element_info[element_index] for element_info in atomic_info if element_info[element_index] == element.capitalize()
+                element_info[element_index]
+                for element_info in atomic_info
+                if element_info[element_index] == element.capitalize()
             ]
         )
         return element_count
@@ -201,10 +213,11 @@ class MoleculeFeaturizer(AbstractFeaturizer):
         ]
         return bonds
 
-    def get_bond_distribution(self, molecule):
-        bonds = self.get_bonds(molecule)
+    def get_bond_distribution(self, molecule=None, molecular_info=None):
+        if molecular_info is None:
+            molecular_info = self.get_bonds(molecule)
 
-        return {bond: bonds.count(bond) for bond in bonds}
+        return {bond: molecular_info.count(bond) for bond in molecular_info}
 
     def get_unique_bond_types(self, molecule):
         bonds = self.get_bonds(molecule)
@@ -214,42 +227,46 @@ class MoleculeFeaturizer(AbstractFeaturizer):
 
 
 if __name__ == "__main__":
-    prob = 0.3
+    prob = 0.2
 
     periodic_table = rdkit.Chem.GetPeriodicTable()
 
     featurizer = MoleculeFeaturizer()
 
-    if prob > .5:
-
+    if prob > 0.5:
         inchi = "InChI=1S/C6H5NO2/c8-7(9)6-4-2-1-3-5-6/h1-5H"
         smiles = "CCC(Cl)C=C"
         selfies_form = encoder(smiles)
-        rep_type = "inchi"
-        mol = Molecule(inchi, rep_type)
+        repr_type = "inchi"
+        mol = Molecule(inchi, repr_type)
 
         print(f"SMILES: {smiles}")
         print(f"SMILES -> SELFIES -> SMILES: {decoder(encoder(smiles))}")
 
         print(featurizer.count_bonds(mol, bond_type="SINGLE"))
-        print(f"Molar mass by featurizer = {featurizer.get_molar_mass(mol)}")
+        print(f"Molar mass by featurizer = {featurizer.get_molar_mass(molecule=mol)}")
         print(f"Bond distribution: {featurizer.get_bond_distribution(mol)}")
         print(featurizer.featurize(mol))
-        print(featurizer.get_element_info(mol, ))
+        print(
+            featurizer.get_element_info(
+                mol,
+            )
+        )
 
     else:
         molecular_info = {
             "InChI=1S/C6H5NO2/c8-7(9)6-4-2-1-3-5-6/h1-5H": "inchi",
             "CCC(Cl)C=C": "smiles",
-            encoder("CCC(Cl)C=C") : "selfies"
+            encoder("CCC(Cl)C=C"): "selfies",
         }
 
-        mols = [
-            Molecule(k, v) for k, v in molecular_info.items()
-        ]
+        mols = [Molecule(k, v) for k, v in molecular_info.items()]
 
         print(featurizer.featurize(mols))
-        print(mols[0].base_string)
+        print(mols[0].repr_string)
 
         element = "n"
-        print(f"Element {element} appears {featurizer.get_element_frequency(molecule=mols[0],element=element)} times")
+        print(
+            f"Element {element} appears {featurizer.get_element_frequency(molecule=mols[0],element=element)} times"
+        )
+        print(mols[0].get_name())
