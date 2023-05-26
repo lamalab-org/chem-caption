324b5161fe4cbb3fd6638c985a425f6d373a4161
Arkhymadhe <emoekabuoshomah@gmail.com>
2023-05-26 11:23:39 +0100

    Attempt to fix Issue #1

--- src/chemcaption/api.py
+++ src/chemcaption/api.py
@@ -1,3 +1,61 @@
 # -*- coding: utf-8 -*-
 
 """Main code."""
+import rdkit
+from rdkit import Chem
+from abc import abstractmethod, ABC
+from fastcore.all import typedispatch
+from multipledispatch import dispatch
+from selfies import decoder, encoder
+
+
+class MoleculeBase(ABC):
+    @abstractmethod
+    def get_rdkit_mol(self):
+        raise NotImplementedError
+
+class SMILESMolecule(MoleculeBase):
+    def __init__(self, smiles):
+        self.smiles = smiles
+
+    def get_rdkit_mol(self):
+        return Chem.MolFromSmiles(self.smiles)
+
+class SELFIESMolecule(MoleculeBase):
+    def __init__(self, selfies):
+        self.selfies = selfies
+        self.smiles_rep = decoder(selfies)
+
+    def get_rdkit_mol(self):
+        return Chem.MolFromSmiles(self.smiles_rep)
+
+class InChIMolecule(MoleculeBase):
+    def __init__(self, inchi):
+        self.inchi = inchi
+
+    def get_rdkit_mol(self):
+        return Chem.MolFromInchi(self.inchi)
+class Molecule(MoleculeBase):
+    def __init__(self, base_string, rep_type="smiles"):
+        self.base_string = base_string
+        self.rep_type = rep_type.lower()
+        self.dispatch_map = {
+            "smiles" : SMILESMolecule,
+            "selfies" : SELFIESMolecule,
+            "inchi" : InChIMolecule,
+        }
+        self.molecule = self.dispatch_map[self.rep_type](self.base_string)
+    def get_rdkit_mol(self):
+        return self.molecule.get_rdkit_mol()
+
+    def get_name(self):
+        return self.rep_type
+
+
+
+if __name__ == "__main__":
+    smiles = "CCC(Cl)C=C"
+    selfies_form = encoder(smiles)
+    mol = Molecule(selfies_form, "selfies")
+
+    print(mol.get_rdkit_mol())
