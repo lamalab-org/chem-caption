6622a2dbb2dd75042032a8d0baee8f45333b5dcb
Arkhymadhe <emoekabuoshomah@gmail.com>
2023-07-08 16:50:13 +0100

    chore (featurizers, presets): Update and make Prompt object deterministic; abstract away prompt template selection

--- src/chemcaption/featurizers.py
+++ src/chemcaption/featurizers.py
@@ -11,7 +11,7 @@ import rdkit
 from rdkit.Chem import rdMolDescriptors
 
 from chemcaption.molecules import InChIMolecule, SELFIESMolecule, SMILESMolecule
-from chemcaption.presets import QA_TEMPLATES, TEXT_TEMPLATES, SMARTSPreset
+from chemcaption.presets import SMARTSPreset, inspect_info
 
 """Abstract classes."""
 
@@ -895,49 +895,78 @@ class SMARTSFeaturizer(AbstractFeaturizer):
 
 @dataclass
 class Prompt:
-    prompt: str
-    completion: Union[str, float, int, bool, List[str], List[float], List[int], List[bool]]
+    """Contain all things prompt-related."""
+
+    completion: Union[str, float, int, bool, List[Union[str, float, int, bool]]]
     representation: Union[str, List[str]]
     representation_type: Union[str, float, int, bool, np.array]
     completion_type: Union[str, float, int, bool, np.array]
+    completion_names: Union[str, List[str]]
     template: Optional[str] = None
 
     def __dict__(self):
+        """Return dictionary representation of object.
+
+        Args:
+            None
+
+        Returns:
+            (dict): Dictionary containing all relevant prompt-related information.
+        """
         return {
-            "prompt": self.prompt,
+            "representation": self.representation,
+            "representation_type": self.representation_type,
+            "prompt": self.template,
             "completion": self.completion,
-            "qa": self.fill_template(template_type="qa"),
-            "text": self.fill_template(template_type="text"),
+            "completion_names": self.completion_names,
+            "filled_prompt": self.fill_template(),
         }
 
-    def fill_template(
-        self, precision_type: str = "decimal", set_template: bool = True, template_type: str = "qa"
-    ):
+    def fill_template(self, precision_type: str = "decimal"):
+        """Fill up the prompt template with appropriate values.
+
+        Args:
+            precision_type (str): Level of precision for approximation purposes. Can be `decimal` r `significant`.
+                Defaults to `decimal`.
+
+        Returns:
+            (str): Appropriately formatted template.
+        """
         molecular_info = dict(
-            PROPERTY_NAME=self.completion_type,
+            PROPERTY_NAME=self.completion_names,
             REPR=self.representation_type,
             REPR_STRING=self.representation,
             PROPERTY_VALUE=self.completion,
-            PRECISION=2,
+            PRECISION=4,
             PRECISION_TYPE=precision_type,
         )
-
-        templates = QA_TEMPLATES if template_type == "qa" else TEXT_TEMPLATES
-        if template_type == "qa":
-            templates = templates[
-                "single" if not isinstance(self.representation, (set, list, tuple)) else "multiple"
-            ]
-
-        self.template = (
-            templates[np.random.randint(low=0, high=len(templates), size=(1,)).item()]
-            if (set_template or bool(self.template))
-            else self.template
-        )
+        molecular_info = inspect_info(molecular_info)
 
         return self.template.format(**molecular_info)
 
     def __str__(self):
-        return self.fill_template(template_type="text")
+        """Return string representation of object.
+
+        Args:
+            None.
+
+        Returns:
+            (str): Appropriately formatted template.
+        """
+        return self.fill_template()
 
     def to_meta_yaml(self):
+        """Convert all prompt information from string to YAML format."""
         raise NotImplementedError
+
+    def implementors(self):
+        """
+        Return list of functionality implementors.
+
+        Args:
+            None
+
+        Returns:
+            List[str]: List of implementors.
+        """
+        return ["Benedict Oshomah Emoekabu"]
