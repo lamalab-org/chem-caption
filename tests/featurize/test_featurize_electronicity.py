b5983a33e447eb7508b8a27db1be6ed43f49986f
Arkhymadhe <emoekabuoshomah@gmail.com>
2023-08-10 17:17:53 +0100

    feat (tests): Implement tests for IsoelectronicComparator

--- tests/featurize/test_featurize_electronicity.py
+++ tests/featurize/test_featurize_electronicity.py
@@ -10,6 +10,9 @@ from chemcaption.featurize.electronicity import (
     HydrogenDonorCountFeaturizer,
     ValenceElectronCountFeaturizer,
 )
+
+from chemcaption.featurize.comparator import IsoelectronicComparator
+
 from tests.conftests import DISPATCH_MAP, PROPERTY_BANK, extract_molecule_properties
 
 KIND = "selfies"
@@ -81,3 +84,34 @@ def test_valence_electron_count_featurizer(test_input, expected):
     results = featurizer.featurize(molecule)
 
     assert np.equal(results, expected).all()
+
+
+"""Test for isoelectronicity featurizer."""
+
+
+@pytest.mark.parametrize(
+    "test_values_1, test_values_2",
+    zip(
+        extract_molecule_properties(
+            property_bank=PROPERTY_BANK, representation_name=KIND, property="num_valence_electrons"
+        ),
+        sorted(
+            extract_molecule_properties(
+                property_bank=PROPERTY_BANK, representation_name=KIND, property="num_valence_electrons"
+            )
+        )
+    ),
+)
+def test_isoelectronic_comparator(test_values_1, test_values_2):
+    """Test IsoelectronicComparator."""
+    test_input_1, expected_1 = test_values_1
+    test_input_2, expected_2 = test_values_2
+
+    featurizer = IsoelectronicComparator()
+
+    molecule_1 = MOLECULE(test_input_1)
+    molecule_2 = MOLECULE(test_input_2)
+
+    results = featurizer.compare([molecule_1, molecule_2])
+
+    assert results == (expected_1.astype(int) == expected_2.astype(int))
