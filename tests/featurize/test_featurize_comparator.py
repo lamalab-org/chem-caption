049c7daa78240ffcd9d3002eed168b707d1e925d
Arkhymadhe <emoekabuoshomah@gmail.com>
2023-08-11 03:58:48 +0100

    feat (tests): Implement tests for IsomerismComparator

--- tests/featurize/test_featurize_comparator.py
+++ tests/featurize/test_featurize_comparator.py
@@ -2,6 +2,7 @@
 
 """Tests for chemcaption.featurize.comparator subpackage."""
 
+import numpy as np
 import pytest
 
 from chemcaption.featurize.comparator import IsoelectronicComparator, ValenceElectronCountComparator, IsomerismComparator
@@ -13,6 +14,7 @@ MOLECULE = DISPATCH_MAP[KIND]
 # Implemented tests for comparator-related featurizers.
 
 __all__ = [
+    "test_isomerism_comparator",
     "test_valence_electron_count_comparator",
     "test_isoelectronicity_comparator",
 ]
@@ -47,6 +49,37 @@ def test_valence_electron_count_comparator(test_values_1, test_values_2):
     results = featurizer.compare([molecule_1, molecule_2])
 
     assert results == (expected_1.astype(int) == expected_2.astype(int))
+
+
+"""Test for isomerism comparator."""
+
+
+@pytest.mark.parametrize(
+    "test_values_1, test_values_2",
+    zip(
+        extract_molecule_properties(
+            property_bank=PROPERTY_BANK, representation_name=KIND, property="molecular_formular"
+        ),
+        sorted(
+            extract_molecule_properties(
+                property_bank=PROPERTY_BANK, representation_name=KIND, property="molecular_formular"
+            )
+        )
+    ),
+)
+def test_isomerism_comparator(test_values_1, test_values_2):
+    """Test IsomerismComparator."""
+    test_input_1, expected_1 = test_values_1
+    test_input_2, expected_2 = test_values_2
+
+    featurizer = IsomerismComparator()
+
+    molecule_1 = MOLECULE(test_input_1)
+    molecule_2 = MOLECULE(test_input_2)
+
+    results = featurizer.compare([molecule_1, molecule_2])
+
+    assert np.equal(results, np.equal(expected_1, expected_2).all()).all()
 
 
 """Test for isoelectronicity comparator."""
