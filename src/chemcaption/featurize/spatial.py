7595066f7d3aeb9e13d1a2a94cbe8c63790bf34d
Arkhymadhe <emoekabuoshomah@gmail.com>
2023-08-28 15:31:36 +0100

    feat (spatial): Implement InertialShapeFactorFeaturizer

--- src/chemcaption/featurize/spatial.py
+++ src/chemcaption/featurize/spatial.py
@@ -13,7 +13,12 @@ from chemcaption.molecules import Molecule
 
 # Implemented bond-related featurizers
 
-__all__ = ["ThreeDimensionalFeaturizer", "EccentricityFeaturizer", "AsphericityFeaturizer"]
+__all__ = [
+    "ThreeDimensionalFeaturizer",
+    "EccentricityFeaturizer",
+    "AsphericityFeaturizer",
+    "InertialShapeFactorFeaturizer",
+]
 
 
 """Abstract Featurizer for extracting 3D features from molecule."""
@@ -136,6 +141,55 @@ class AsphericityFeaturizer(ThreeDimensionalFeaturizer):
         _ = EmbedMolecule(mol)
 
         asphericity_value = Descriptors3D.Asphericity(
+            mol, confId=self.conformer_id, force=self.force, useAtomicMasses=self.use_masses
+        )
+        return np.array([asphericity_value]).reshape(1, -1)
+
+    def implementors(self) -> List[str]:
+        """
+        Return list of functionality implementors.
+
+        Args:
+            None.
+
+        Returns:
+            List[str]: List of implementors.
+        """
+        return ["Benedict Oshomah Emoekabu"]
+
+
+"""Abstract Featurizer for extracting asphericity property from molecule."""
+
+
+class InertialShapeFactorFeaturizer(ThreeDimensionalFeaturizer):
+    """Featurizer to return inertia shape factor of a molecule."""
+
+    def __init__(self, conformer_id: Optional[int] = -1, use_masses: bool = True, force=True):
+        """Initialize class object.
+
+        Args:
+            conformer_id (Optional[int]): Integer identifier for molecule conformation. Defaults to `-1`.
+            use_masses (bool): Utilize elemental masses in calculation of inertia shape factor. Defaults to `True`.
+            force (bool):
+        """
+        super().__init__(conformer_id=conformer_id, use_masses=use_masses, force=force)
+
+        self.label = ["inertia_shape_factor"]
+
+    def featurize(self, molecule: Molecule) -> np.array:
+        """
+        Featurize single molecule instance. Extract inertia shape factor for `molecule`.
+
+        Args:
+            molecule (Molecule): Molecule representation.
+
+        Returns:
+            (np.array): Array containing inertia shape factor.
+        """
+        mol = molecule.reveal_hydrogens()
+        _ = EmbedMolecule(mol)
+
+        asphericity_value = Descriptors3D.InertialShapeFactor(
             mol, confId=self.conformer_id, force=self.force, useAtomicMasses=self.use_masses
         )
         return np.array([asphericity_value]).reshape(1, -1)
