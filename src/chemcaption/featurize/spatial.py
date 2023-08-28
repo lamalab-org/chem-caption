2e5f43f2f32c511f092b445e0bd9376cdc2a6ccd
Arkhymadhe <emoekabuoshomah@gmail.com>
2023-08-28 15:22:31 +0100

    feat (spatial): Implement AsphericityFeaturizer

--- src/chemcaption/featurize/spatial.py
+++ src/chemcaption/featurize/spatial.py
@@ -5,7 +5,7 @@
 from typing import List, Optional
 
 import numpy as np
-from rdkit.Chem import Descriptors3D, rdMolDescriptors
+from rdkit.Chem import Descriptors3D
 from rdkit.Chem.AllChem import EmbedMolecule
 
 from chemcaption.featurize.base import AbstractFeaturizer
@@ -13,30 +13,60 @@ from chemcaption.molecules import Molecule
 
 # Implemented bond-related featurizers
 
-__all__ = [
-    "EccentricityFeaturizer",
-]
+__all__ = ["ThreeDimensionalFeaturizer", "EccentricityFeaturizer", "AsphericityFeaturizer"]
+
+
+"""Abstract Featurizer for extracting 3D features from molecule."""
 
 
 class ThreeDimensionalFeaturizer(AbstractFeaturizer):
+    """Abstract class for 3-D featurizers."""
 
     def __init__(self, conformer_id: Optional[int] = -1, use_masses: bool = True, force=True):
+        """Instantiate initialization scheme to be inherited."""
         super().__init__()
 
         self.conformer_id = conformer_id
         self.use_masses = use_masses
         self.force = force
 
+    def featurize(self, molecule: Molecule) -> None:
+        """
+        Featurize single molecule instance. Extract eccentricity value for `molecule`.
+
+        Args:
+            molecule (Molecule): Molecule representation.
+
+        Returns:
+            None.
+        """
+        pass
+
+    def implementors(self) -> List[str]:
+        """
+        Return list of functionality implementors.
+
+        Args:
+            None.
+
+        Returns:
+            List[str]: List of implementors.
+        """
+        return ["Benedict Oshomah Emoekabu"]
+
+
+"""Abstract Featurizer for extracting eccentricity property from molecule."""
+
 
 class EccentricityFeaturizer(ThreeDimensionalFeaturizer):
-    """Featurizer to return number of unique `element` environments in a molecule."""
+    """Featurizer to return eccentricity value of a molecule."""
 
     def __init__(self, conformer_id: Optional[int] = -1, use_masses: bool = True, force=True):
         """Initialize class object.
 
         Args:
             conformer_id (Optional[int]): Integer identifier for molecule conformation. Defaults to `-1`.
-            use_masses (bool): Utilize elemental masses in asphericity calculation. Defaults to `True`.
+            use_masses (bool): Utilize elemental masses in eccentricity calculation. Defaults to `True`.
             force (bool):
         """
         super().__init__(conformer_id=conformer_id, use_masses=use_masses, force=force)
@@ -55,10 +85,60 @@ class EccentricityFeaturizer(ThreeDimensionalFeaturizer):
         """
         mol = molecule.reveal_hydrogens()
         _ = EmbedMolecule(mol)
+
         eccentricity_value = Descriptors3D.Eccentricity(
-            mol
+            mol, confId=self.conformer_id, force=self.force, useAtomicMasses=self.use_masses
         )
         return np.array([eccentricity_value]).reshape(1, -1)
+
+    def implementors(self) -> List[str]:
+        """
+        Return list of functionality implementors.
+
+        Args:
+            None.
+
+        Returns:
+            List[str]: List of implementors.
+        """
+        return ["Benedict Oshomah Emoekabu"]
+
+
+"""Abstract Featurizer for extracting asphericity property from molecule."""
+
+
+class AsphericityFeaturizer(ThreeDimensionalFeaturizer):
+    """Featurizer to return number of asphericity value of a molecule."""
+
+    def __init__(self, conformer_id: Optional[int] = -1, use_masses: bool = True, force=True):
+        """Initialize class object.
+
+        Args:
+            conformer_id (Optional[int]): Integer identifier for molecule conformation. Defaults to `-1`.
+            use_masses (bool): Utilize elemental masses in asphericity calculation. Defaults to `True`.
+            force (bool):
+        """
+        super().__init__(conformer_id=conformer_id, use_masses=use_masses, force=force)
+
+        self.label = ["asphericity"]
+
+    def featurize(self, molecule: Molecule) -> np.array:
+        """
+        Featurize single molecule instance. Extract asphericity value for `molecule`.
+
+        Args:
+            molecule (Molecule): Molecule representation.
+
+        Returns:
+            (np.array): Array containing asphericity value.
+        """
+        mol = molecule.reveal_hydrogens()
+        _ = EmbedMolecule(mol)
+
+        asphericity_value = Descriptors3D.Asphericity(
+            mol, confId=self.conformer_id, force=self.force, useAtomicMasses=self.use_masses
+        )
+        return np.array([asphericity_value]).reshape(1, -1)
 
     def implementors(self) -> List[str]:
         """
