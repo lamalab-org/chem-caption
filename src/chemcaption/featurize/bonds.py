80419db02f816063a79931f0587b6e94bef8734b
Arkhymadhe <emoekabuoshomah@gmail.com>
2023-09-18 22:39:22 +0100

    feat (bonds): Implement DipoleMomentsFeaturizer

--- src/chemcaption/featurize/bonds.py
+++ src/chemcaption/featurize/bonds.py
@@ -2,12 +2,12 @@
 
 """Featurizers for chemical bond-related information."""
 
-from typing import List, Union
+from typing import Any, Dict, List, Optional, Union
 
 import numpy as np
 from rdkit.Chem import rdMolDescriptors
 
-from chemcaption.featurize.base import AbstractFeaturizer
+from chemcaption.featurize.base import AbstractFeaturizer, MorfeusFeaturizer
 from chemcaption.featurize.utils import join_list_elements
 from chemcaption.molecules import Molecule
 
@@ -18,6 +18,7 @@ __all__ = [
     "BondRotabilityFeaturizer",
     "BondTypeCountFeaturizer",
     "BondTypeProportionFeaturizer",
+    "DipoleMomentsFeaturizer",
 ]
 
 
@@ -417,3 +418,85 @@ class BondTypeProportionFeaturizer(BondTypeCountFeaturizer):
             (np.array): Array containing bond type proportion(s).
         """
         return np.array(self._get_bond_distribution(molecule=molecule)).reshape(1, -1)
+
+
+class DipoleMomentsFeaturizer(MorfeusFeaturizer):
+    """Return the dipole moments for a molecule."""
+
+    def __init__(
+        self,
+        file_name: Optional[str] = None,
+        conformer_generation_kwargs: Optional[Dict[str, Any]] = None,
+        morfeus_kwargs: Optional[Dict[str, Any]] = None,
+        atom_indices: Union[int, List[int]] = 100,
+        as_range: bool = False,
+    ):
+        """Instantiate class.
+
+        Args:
+            file_name (Optional[str]): Name for temporary XYZ file.
+            conformer_generation_kwargs (Optional[Dict[str, Any]]): Configuration for conformer generation.
+            morfeus_kwargs (Optional[Dict[str, Any]]): Keyword arguments for morfeus computation.
+            atom_indices (Union[int, List[int]]): Range of atoms to calculate areas for. Either:
+                - an integer,
+                - a list of integers, or
+                - a two-tuple of integers representing lower index and upper index.
+            as_range (bool): Use `atom_indices` parameter as a range of indices or not. Defaults to `False`
+        """
+        super().__init__(
+            file_name=file_name,
+            conformer_generation_kwargs=conformer_generation_kwargs,
+            morfeus_kwargs=morfeus_kwargs,
+        )
+
+        self._names = [
+            {
+                "noun": "dipole moments",
+            },
+        ]
+
+        self.atom_indices, self.as_range = self._parse_indices(atom_indices, as_range)
+
+    def featurize(self, molecule: Molecule) -> np.array:
+        """
+        Featurize single molecule instance.
+
+        Args:
+            molecule (Molecule): Molecule representation.
+
+        Returns:
+            (np.array): Array containing dipole moments for bonds in molecule instance.
+        """
+        morfeus_instance = self._get_morfeus_instance(molecule=molecule, morpheus_instance="xtb")
+
+        dipoles = morfeus_instance.get_dipole(**self.morfeus_kwargs).flatten().tolist()
+        num_dipoles = len(dipoles)
+
+        print(num_dipoles)
+
+        dipoles = [(dipoles[i - 1] if i <= num_dipoles else 0) for i in self.atom_indices]
+
+        return np.array(dipoles).reshape(1, -1)
+
+    def feature_labels(self) -> List[str]:
+        """Return feature label(s).
+
+        Args:
+            None.
+
+        Returns:
+            (List[str]): List of names of extracted features.
+        """
+        return [f"dipole_{i}" for i in self.atom_indices]
+
+    def implementors(self) -> List[str]:
+        """
+        Return list of functionality implementors.
+
+        Args:
+            None.
+
+        Returns:
+            List[str]: List of implementors.
+        """
+        return ["Benedict Oshomah Emoekabu"]
