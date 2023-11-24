d26ed65f8de844c4d1b5e5bc42d77f05e585c9db
Arkhymadhe <emoekabuoshomah@gmail.com>
2023-11-24 21:33:25 +0100

    feat (tests): Implement regression test for LeadLikenessFilterFeaturizer

--- tests/regression/featurize/test_featurize_rules.py
+++ tests/regression/featurize/test_featurize_rules.py
@@ -23,6 +23,7 @@ PRESET = ["carbon", "hydrogen", "oxygen", "nitrogen", "phosphorus"]
 __all__ = [
     "test_lipinski_filter_featurizer",
     "test_ghose_filter_featurizer",
+    "test_leadlikeness_filter_featurizer",
 ]
 
 
@@ -57,6 +58,27 @@ def test_lipinski_filter_featurizer(test_input, expected):
 def test_ghose_filter_featurizer(test_input, expected):
     """Test GhoseFilterFeaturizer."""
     featurizer = GhoseFilterFeaturizer()
+    molecule = MOLECULE(test_input)
+
+    results = featurizer.featurize(molecule)
+
+    assert np.equal(results, expected).all()
+
+
+"""Test for lead-likeness violation count featurizer."""
+
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    extract_molecule_properties(
+        property_bank=PROPERTY_BANK,
+        representation_name=KIND,
+        property="num_lead_likeness_violations",
+    ),
+)
+def test_leadlikeness_filter_featurizer(test_input, expected):
+    """Test LeadLikenessFilterFeaturizer."""
+    featurizer = LeadLikenessFilterFeaturizer()
     molecule = MOLECULE(test_input)
 
     results = featurizer.featurize(molecule)
